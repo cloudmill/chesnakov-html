120cf70c1e1c8526a9b621c7be52dc39953b5351
szatolokin <staszatolokin28@gmail.com>
2021-12-29 15:44:56 +0300

    Update project

--- src/scripts/choice.js
+++ src/scripts/choice.js
@@ -1,38 +1,32 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const DROP_ACTIVE_CLASS = "choice__drop--active";
+  const allChoice = document.querySelectorAll(".choice");
 
-  window.addEventListener("click", (e) => {
-    const panel = e.target.closest(".choice__panel");
+  allChoice.forEach((choice) => {
+    let isOpen = choice.classList.contains("choice--open");
 
-    if (panel) {
-      const choice = panel.closest(".choice");
-      const drop = choice.querySelector(".choice__drop");
-      const isOpen = drop.classList.contains(DROP_ACTIVE_CLASS);
+    const panel = choice.querySelector(".choice__panel");
 
+    const drop = choice.querySelector(".choice__drop");
+    const dropContent = choice.querySelector(".choice__drop-content");
+
+    panel.addEventListener("click", () => {
+      choice.classList.toggle("choice--open");
+
+      const dropContentHeight = isOpen
+        ? 0
+        : dropContent.getBoundingClientRect().height;
+      drop.style.height = `${dropContentHeight}px`;
+
+      isOpen = !isOpen;
+    });
+
+    window.addEventListener("resize", () => {
       if (isOpen) {
-        drop.style.height = 0;
-
-        drop.classList.remove(DROP_ACTIVE_CLASS);
-      } else {
-        const dropContent = drop.querySelector(".choice__drop-content");
         const dropContentHeight = dropContent.getBoundingClientRect().height;
+
         drop.style.height = `${dropContentHeight}px`;
 
-        drop.classList.add(DROP_ACTIVE_CLASS);
-      }
-    }
-  });
-
-  window.addEventListener("resize", () => {
-    const drops = document.querySelectorAll(".choice__drop");
-
-    Array.from(drops).forEach((drop) => {
-      const isOpen = drop.classList.contains(DROP_ACTIVE_CLASS);
-
-      if (isOpen) {
-        const dropContent = drop.querySelector(".choice__drop-content");
-        const dropContentHeight = dropContent.getBoundingClientRect().height;
-        drop.style.height = `${dropContentHeight}px`;
+        console.log(`${dropContentHeight}px`);
       }
     });
   });
