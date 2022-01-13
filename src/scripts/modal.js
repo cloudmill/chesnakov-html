6e729f2e0ff7dab897a9111eafad0c5ebcef744a
szatolokin <staszatolokin28@gmail.com>
2022-01-13 12:10:08 +0300

    Update project

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,32 +1,56 @@
 import { onSignal } from "./signal";
 
 $(() => {
-  $(window).on("click", (e) => {
-    if (
-      $(".modal").hasClass("modal--active") &&
-      !$(e.target).closest(".modal__popup").length
-    ) {
-      $(".modal").removeClass("modal--active");
-    }
-  });
+  const modal = $(".modal");
 
-  $("[data-modal-open]").on("click", () => {
-    if (!$(".modal").hasClass("modal--active")) {
-      setTimeout(() => {
-        $(".modal").addClass("modal--active");
+  function handleWheel(e) {
+    e.preventDefault();
+  }
+
+  if (modal.length) {
+    const modalForm = $(".modal__form");
+
+    $(window).on("click", (e) => {
+      const popupClick = !!$(e.target).closest(".modal__popup").length;
+
+      const openClick = !!$(e.target).closest("[data-modal-open]").length;
+      const closeClick = !!$(e.target).closest("[data-modal-close]").length;
+
+      const isActive = modal.hasClass("modal--active");
+      const isResponse = modal.hasClass("modal--response");
+
+      console.log("modal", {
+        popupClick,
+        openClick,
+        closeClick,
+        isActive,
+        isResponse,
       });
-    }
-  });
 
-  $("[data-modal-close]").on("click", () => {
-    if ($(".modal").hasClass("modal--active")) {
-      setTimeout(() => {
-        $(".modal").removeClass("modal--active");
-      });
-    }
-  });
+      if (isActive) {
+        if (closeClick || !popupClick) {
+          modal.removeClass("modal--active");
 
-  onSignal("modal-response-open", () => {
-    $(".modal").addClass("modal--response");
-  });
+          window.removeEventListener("wheel", handleWheel, { passive: false });
+
+          if (isResponse) {
+            modal.removeClass("modal--response");
+
+            modalForm.parsley().reset();
+            modalForm[0].reset();
+          }
+        }
+      } else {
+        if (openClick) {
+          modal.addClass("modal--active");
+
+          window.addEventListener("wheel", handleWheel, { passive: false });
+        }
+      }
+    });
+
+    onSignal("modal-response-open", () => {
+      modal.addClass("modal--response");
+    });
+  }
 });
