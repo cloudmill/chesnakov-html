1ffb5e4ff454544abfa4d80ba1fb8066b6582e69
szatolokin <staszatolokin28@gmail.com>
2022-01-19 13:05:18 +0300

    Update project

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -1,41 +1,90 @@
 import { sendSignal } from "./signal";
 
 $(() => {
+  function validateCheckboxes(checkboxes, addError, removeError) {
+    const checkboxesInputs = checkboxes.find("[type=checkbox]");
+
+    let checkCount = 0;
+    checkboxesInputs.each(function () {
+      const checkboxInput = $(this);
+      checkCount += +checkboxInput.is(":checked");
+    });
+
+    if (checkCount === 0) {
+      setTimeout(() => {
+        checkboxes.addClass("checkboxes--error");
+      });
+    } else {
+      setTimeout(() => {
+        checkboxes.removeClass("checkboxes--error");
+      });
+    }
+  }
+
+  $("form").each(function () {
+    const form = $(this);
+    const checkboxes = form.find(".checkboxes");
+    const checkboxesInputs = checkboxes.find("[type=checkbox]");
+
+    if (checkboxes.length) {
+      form.parsley().on("form:validate", () => validateCheckboxes(checkboxes));
+      checkboxesInputs.on("change", () => validateCheckboxes(checkboxes));
+    }
+  });
+
   $("form").on("submit", function (e) {
     e.preventDefault();
 
-    const url = $(this).data("url");
-    const successStatus = $(this).data("success-status");
-    const successAction = $(this).data("success-action");
-    const params = $(this).serialize();
-    const isTest = $(this)[0].hasAttribute("data-form-test");
+    setTimeout(() => {
+      const url = $(this).data("url");
+      const successStatus = $(this).data("success-status");
+      const successAction = $(this).data("success-action");
+      const params = $(this).serialize();
+      const isTest = $(this)[0].hasAttribute("data-form-test");
 
-    console.log("form", this, {
-      url,
-      successStatus,
-      successAction,
-      params,
-      isTest,
-    });
+      const checkboxes = $(this).find(".checkboxes");
+      let checkboxesValidated;
 
-    try {
-      if (isTest) {
-        setTimeout(() => {
-          successAction && sendSignal(successAction);
-        }, 1000);
+      if (checkboxes.length) {
+        if (!checkboxes.hasClass("checkboxes--error")) {
+          checkboxesValidated = true;
+        } else {
+          checkboxesValidated = false;
+        }
+      } else {
+        checkboxesValidated = true;
       }
 
-      $.post(url, params, (data) => {
-        if (data.status === successStatus) {
-          successAction && sendSignal(successAction);
-
-          console.log("form", this, "success");
-        } else {
-          console.log("form", this, "fail");
-        }
+      console.log("form", this, {
+        url,
+        successStatus,
+        successAction,
+        params,
+        isTest,
+        checkboxesValidated,
       });
-    } catch (error) {
-      console.error("form", this, error);
-    }
+
+      if (checkboxesValidated) {
+        try {
+          if (isTest) {
+            setTimeout(() => {
+              successAction && sendSignal(successAction);
+            }, 1000);
+          }
+
+          $.post(url, params, (data) => {
+            if (data.status === successStatus) {
+              successAction && sendSignal(successAction);
+
+              console.log("form", this, "success");
+            } else {
+              console.log("form", this, "fail");
+            }
+          });
+        } catch (error) {
+          console.error("form", this, error);
+        }
+      }
+    });
   });
 });
